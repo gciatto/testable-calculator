0870dad3e3b52b24c36a99b453c4c154738e40c7
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-04-03 13:12:53 +0200

    test: add gui tests for square root, power, and parentheses

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -23,6 +23,17 @@ class CalculatorGUITestCase(unittest.TestCase):
 
     def tearDown(self):
         self.app.stop()
+
+
+class TestLayout(CalculatorGUITestCase):
+
+    BUTTONS_TO_TEST = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+                       "+", "-", "*", "/", ".", "C", "="]
+
+    def test_all_buttons_are_there(self):
+        for button_text in self.BUTTONS_TO_TEST:
+            with self.subTest(button=button_text):
+                self.assert_button_exists(button_text)
     
 
 class TestExpressions(CalculatorGUITestCase):
@@ -44,12 +55,50 @@ class TestExpressions(CalculatorGUITestCase):
         self.press_button("=")
         self.assert_display("3.2")
 
-class TestLayout(CalculatorGUITestCase):
+    def test_complex_expressions(self):
+        # test that (1+2)*3 == 9
+        self.press_button("(")
+        self.assert_display("(")
+        self.press_button("1")
+        self.assert_display("(1")
+        self.press_button("+")
+        self.assert_display("(1+")
+        self.press_button("2")
+        self.press_button(")")
+        self.press_button("*")
+        self.press_button("3")
+        self.assert_display("(1+2)*3")
+        self.press_button("=")
+        self.assert_display("9")
 
-    BUTTONS_TO_TEST = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-                       "+", "-", "*", "/", ".", "C", "="]
+    def test_sqrt_expression(self):
+        # sqrt(5 - 1) + 1 == 3
+        self.press_button("√")
+        self.assert_display("√(")
+        self.press_button("5")
+        self.press_button("-")
+        self.press_button("1")
+        self.press_button(")")
+        self.press_button("+")
+        self.press_button("1")
+        self.assert_display("√(5-1)+1")
+        self.press_button("=")
+        self.assert_display("3.0")
 
-    def test_all_buttons_are_there(self):
-        for button_text in self.BUTTONS_TO_TEST:
-            with self.subTest(button=button_text):
-                self.assert_button_exists(button_text)
+    def test_power_expression(self):
+        # (4 / 2) ** (5 - 1) == 16
+        self.press_button("(")
+        self.press_button("4")
+        self.press_button("/")
+        self.press_button("2")
+        self.press_button(")")
+        self.assert_display("(4/2)")
+        self.press_button("^") # ^ is the symbol for power that we wanna have in our GUI
+        self.press_button("(")
+        self.press_button("5")
+        self.press_button("-")
+        self.press_button("1")
+        self.press_button(")")
+        self.assert_display("(4/2)^(5-1)")
+        self.press_button("=")
+        self.assert_display("16")
