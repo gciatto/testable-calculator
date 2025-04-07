cbe23f906e869854fe67384c4ac9654dfc15610b
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2025-04-07 15:55:28 +0200

    test: add gui tests for square root, power, and parentheses

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -28,7 +28,7 @@ class CalculatorGUITestCase(unittest.TestCase):
 class TestLayout(CalculatorGUITestCase):
 
     BUTTONS_TO_TEST = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-                       "+", "-", "*", "/", ".", "C", "="]
+                       "+", "-", "*", "/", ".", "C", "=", "(", ")", "√", "^"]
 
     def test_all_buttons_are_there(self):
         for button_text in self.BUTTONS_TO_TEST:
@@ -54,3 +54,51 @@ class TestExpressions(CalculatorGUITestCase):
         self.assert_display("1.2+2")
         self.press_button("=")
         self.assert_display("3.2")
+
+    def test_complex_expressions(self):
+        # test that (1+2)*3 == 9
+        self.press_button("(")
+        self.assert_display("(")
+        self.press_button("1")
+        self.assert_display("(1")
+        self.press_button("+")
+        self.assert_display("(1+")
+        self.press_button("2")
+        self.press_button(")")
+        self.press_button("*")
+        self.press_button("3")
+        self.assert_display("(1+2)*3")
+        self.press_button("=")
+        self.assert_display("9")
+
+    def test_sqrt_expression(self):
+        # √(5 - 1) + 1 == 3
+        self.press_button("√")
+        self.assert_display("√(")
+        self.press_button("5")
+        self.press_button("-")
+        self.press_button("1")
+        self.press_button(")")
+        self.press_button("+")
+        self.press_button("1")
+        self.assert_display("√(5-1)+1")
+        self.press_button("=")
+        self.assert_display("3.0")
+
+    def test_power_expression(self):
+        # (4 / 2) ^ (5 - 1) == 16
+        self.press_button("(")
+        self.press_button("4")
+        self.press_button("/")
+        self.press_button("2")
+        self.press_button(")")
+        self.assert_display("(4/2)")
+        self.press_button("^") # ^ is the symbol for power that we wanna have in our GUI
+        self.press_button("(")
+        self.press_button("5")
+        self.press_button("-")
+        self.press_button("1")
+        self.press_button(")")
+        self.assert_display("(4/2)^(5-1)")
+        self.press_button("=")
+        self.assert_display("16")
